383ad60ab3041e0a96fed2c0aaad1167d90abd7d
chrisaddy <chris.william.addy@gmail.com>
2024-10-14 11:23:40 -0400

    add flake

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "tyrell development environment";
+  
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ...}:
+  flake-utils.lib.eachDefaultSystem (system: 
+    let
+      overlays = [ (import rust-overlay) ];
+
+      pkgs = import nixpkgs {
+        inherit system overlays;
+      };
+      rustVersion = pkgs.rust-bin.stable.latest.default;
+    in
+    {
+      devShells.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          rustVersion
+          cargo
+          rustc
+          rustfmt
+          rust-analyzer
+          clippy
+          openssl
+          pkg-config
+        ];
+        shellHook = ''
+          echo "rust version: $(rustc --version)"
+        '';
+      };
+    }
+  );
+}
